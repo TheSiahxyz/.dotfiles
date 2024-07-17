cb30b0b6174d3ce335155a81c064960263daa25c
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-07-18 03:30:03 +0900

    Init

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/molten.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/molten.lua
@@ -90,24 +90,30 @@ return {
 		dependencies = { "3rd/image.nvim" },
 		build = ":UpdateRemotePlugins",
 		init = function()
-			vim.g.molten_output_win_max_height = 20
+			vim.g.molten_auto_image_popup = true
+			vim.g.molten_auto_init_behavior = "raise"
+			vim.g.molten_auto_open_html_in_browser = false
 			-- I find auto open annoying, keep in mind setting this option will require setting
 			-- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
-			vim.g.molten_auto_open_output = false
-
+			vim.g.molten_auto_open_output = true
+			vim.g.molten_cover_empty_lines = false
+			vim.g.molten_cover_lines_starting_with = {}
+			vim.g.molten_copy_output = false
+			vim.g.molten_enter_output_behavior = "open_then_enter"
 			-- this guide will be using image.nvim
 			-- Don't forget to setup and install the plugin if you want to view image outputs
 			vim.g.molten_image_provider = "image.nvim"
-
-			-- optional, I like wrapping. works for virt text and the output window
-			vim.g.molten_wrap_output = true
-
+			vim.g.molten_output_show_more = false
+			vim.g.molten_output_win_max_height = 30
+            vim.g.molten_output_win_style = "minimal"
+			-- this will make it so the output shows up below the \`\`\` cell delimiter
+			vim.g.molten_virt_lines_off_by_1 = true
 			-- Output as virtual text. Allows outputs to always be shown, works with images, but can
 			-- be buggy with longer images
 			vim.g.molten_virt_text_output = true
-
-			-- this will make it so the output shows up below the \`\`\` cell delimiter
-			vim.g.molten_virt_lines_off_by_1 = true
+			-- optional, works for virt text and the output window
+			vim.g.molten_wrap_output = true
+            vim.g.molten_virt_text_max_lines = 20
 		end,
 		config = function()
 			-- image nvim options table. Pass to `require('image').setup`
@@ -136,7 +142,7 @@ return {
 				else
 					vim.cmd("MoltenInit python3")
 				end
-			end, { desc = "Initialize Molten for python3", silent = true })
+			end)
 		end,
 	},
 }
