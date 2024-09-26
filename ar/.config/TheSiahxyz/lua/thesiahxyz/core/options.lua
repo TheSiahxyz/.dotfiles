c789664d43bf5b4f227a1184f946ef75e1ec1862
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-09-26 17:41:47 +0900

    Init

--- ar/.config/TheSiahxyz/lua/thesiahxyz/core/options.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/core/options.lua
@@ -3,6 +3,7 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 1
 vim.g.netrw_browsex_viewer = "xdg-open"
 vim.g.netrw_liststyle = 0
+vim.g.netrw_list_hide = "^.git"
 vim.g.netrw_winsize = 25
 vim.opt.backup = false
 vim.opt.breakindent = true
