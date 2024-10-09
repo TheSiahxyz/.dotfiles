45b9719dc691a396ecc44205221d8a16817b49a7
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-10-09 17:45:08 +0900

    updated

--- ar/.config/zsh/scripts.zsh
+++ ar/.config/zsh/scripts.zsh
@@ -330,9 +330,9 @@ function check_git_repos_status() {
                 cd "$DIR" || continue
 
                 if [ "$(dirname $DIR)" = ".password-store" ]; then
-                    pass git fetch
+                    pass git fetch >/dev/null 2>&1
                 else
-                    git fetch --all --prune --jobs=10
+                    git fetch --all --prune --jobs=10 >/dev/null 2>&1
                 fi
                 # Get Git branch and status using __git_ps1
                 GIT_STATUS=$(__git_ps1 "%s")
