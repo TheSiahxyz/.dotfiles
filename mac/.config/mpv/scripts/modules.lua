8180d90473bee35ac977dea90dac5fa74732fb51
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-10-21 21:57:25 +0900

    updates

--- mac/.config/mpv/scripts/modules.lua
+++
@@ -1,5 +0,0 @@
-local mpv_config_dir_path = require("mp").command_native({ "expand-path", "~~/" })
-function load(relative_path)
-	dofile(mpv_config_dir_path .. "/script-modules/" .. relative_path)
-end
-load("mpvSockets.lua")
