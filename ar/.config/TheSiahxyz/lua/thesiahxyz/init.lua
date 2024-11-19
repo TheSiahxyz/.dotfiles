6d206d12817524127f7fec86b000e6818a2d63e6
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-11-19 22:42:54 +0900

    modified init.lua, modified which-key.lua, created utils, created opentasktui

--- ar/.config/TheSiahxyz/lua/thesiahxyz/init.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/init.lua
@@ -4,6 +4,11 @@ require("thesiahxyz.core.keymaps")
 require("thesiahxyz.core.options")
 require("thesiahxyz.core.lazy")
 
+-- Custom
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/thesiahxyz/utils", [[v:val =~ '\.lua$']])) do
+	require("thesiahxyz.utils." .. file:gsub("%.lua$", ""))
+end
+
 -- Plenary
 function R(name)
 	require("plenary.reload").reload_module(name)
