cece827fcc6380cbf42d47989cea08182317809a
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-13 13:11:45 +0900

    modified csv.lua, modified treesitter.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/csv.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/csv.lua
@@ -1,35 +1,70 @@
 return {
-	"hat0uma/csvview.nvim",
-	cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
-	event = { "BufReadPre *.csv" }, -- Lazy-load the plugin when a CSV file is about to be read
-	config = function()
-		require("csvview").setup()
+	{
+		"cameron-wags/rainbow_csv.nvim",
+		config = function()
+			require("rainbow_csv").setup()
+			-- vim.g.rcsv_colorpairs = {
+			-- 	{ "red", "red" },
+			-- 	{ "blue", "blue" },
+			-- 	{ "green", "green" },
+			-- 	{ "magenta", "magenta" },
+			-- 	{ "NONE", "NONE" },
+			-- 	{ "darkred", "darkred" },
+			-- 	{ "darkblue", "darkblue" },
+			-- 	{ "darkgreen", "darkgreen" },
+			-- 	{ "darkmagenta", "darkmagenta" },
+			-- 	{ "darkcyan", "darkcyan" },
+			-- }
+		end,
+		ft = {
+			"csv",
+			"tsv",
+			"csv_semicolon",
+			"csv_whitespace",
+			"csv_pipe",
+			"rfc_csv",
+			"rfc_semicolon",
+		},
+		cmd = {
+			"RainbowDelim",
+			"RainbowDelimSimple",
+			"RainbowDelimQuoted",
+			"RainbowMultiDelim",
+		},
+	},
+	{
+		"hat0uma/csvview.nvim",
+		cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+		event = { "BufReadPre *.csv" }, -- Lazy-load the plugin when a CSV file is about to be read
+		config = function()
+			require("csvview").setup()
 
-		vim.api.nvim_create_autocmd("BufRead", {
-			pattern = "*.csv",
-			callback = function()
-				pcall(vim.cmd, "CsvViewEnable") -- Call CsvViewEnable safely
-			end,
-		})
-	end,
-	keys = {
-		{ "<leader>tv", "<cmd>CsvViewToggle<cr>", desc = "Toggle CSV view" },
-		{
-			"<leader>csv",
-			function()
-				local delimiter = vim.fn.input("Delimiter (e.g., ,): ")
-				local quote_char = vim.fn.input("Quote char (e.g., '): ")
-				local comment = vim.fn.input("Comment char (e.g., #): ")
-				local command = string.format(
-					":CsvViewToggle delimiter=%s quote_char=%s comment=%s<CR>",
-					delimiter,
-					quote_char,
-					comment
-				)
+			vim.api.nvim_create_autocmd("BufRead", {
+				pattern = "*.csv",
+				callback = function()
+					pcall(vim.cmd, "CsvViewEnable") -- Call CsvViewEnable safely
+				end,
+			})
+		end,
+		keys = {
+			{ "<leader>tv", "<cmd>CsvViewToggle<cr>", desc = "Toggle CSV view" },
+			{
+				"<leader>csv",
+				function()
+					local delimiter = vim.fn.input("Delimiter (e.g., ,): ")
+					local quote_char = vim.fn.input("Quote char (e.g., '): ")
+					local comment = vim.fn.input("Comment char (e.g., #): ")
+					local command = string.format(
+						":CsvViewToggle delimiter=%s quote_char=%s comment=%s<CR>",
+						delimiter,
+						quote_char,
+						comment
+					)
 
-				vim.cmd(command)
-			end,
-			desc = "Toggle CSV view",
+					vim.cmd(command)
+				end,
+				desc = "Toggle CSV view",
+			},
 		},
 	},
 }
--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/treesitter.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/treesitter.lua
@@ -34,7 +34,7 @@ return {
 			highlight = {
 				-- `false` will disable the whole extension
 				enable = true,
-
+				disable = { "csv" },
 				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
 				-- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -55,6 +55,6 @@ return {
 		vim.treesitter.language.register("templ", "templ")
 	end,
 	keys = {
-		{ "<leader>tu", ":TSUpdate<cr>", desc = "Tree sitter update" },
+		{ "<leader>tu", ":TSUpdate<cr>", desc = "Update tree sitter" },
 	},
 }
