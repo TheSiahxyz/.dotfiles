bfdc9b8450e4b4c319006c75331cfcaf43ee8efc
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-17 23:35:14 +0900

    modified init.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/init.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/init.lua
@@ -9,7 +9,7 @@ return {
 			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = "Tmux navigator down" })
 			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = "Tmux navigator up" })
 			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = "Tmux navigator right" })
-			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { desc = "Tmux navigator prev" })
+			vim.keymap.set("n", "<A-p>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { desc = "Tmux navigator prev" })
 			vim.keymap.set("n", "<A-n>", nvim_tmux_nav.NvimTmuxNavigateNext, { desc = "Tmux navigator next" })
 		end,
 	},
