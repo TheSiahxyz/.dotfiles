239c511ba8edbe706476fd7872ac92424d5f9df5
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-17 13:48:45 +0900

    modified cloak.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/cloak.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/cloak.lua
@@ -16,11 +16,12 @@ return {
 						"wrangler.toml",
 						".dev.vars",
 						"address*",
+						"*api*",
 					},
 					-- Match an equals sign and any character after it.
 					-- This can also be a table of patterns to cloak,
 					-- example: cloak_pattern = { ":.+", "-.+" } for yaml files.
-					cloak_pattern = "=.+",
+					cloak_pattern = { "=.+", ":.+", "-.+" },
 				},
 			},
 		})
