281a82138f19fa856799edf2016559b499a93e29
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-30 16:26:11 +0900

    modified noice.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/noice.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/noice.lua
@@ -4,14 +4,59 @@ return {
 	config = function()
 		require("noice").setup({
 			cmdline = {
-				enabled = false, -- Disable the Noice cmdline UI
+				enabled = true, -- enables the Noice cmdline UI
+				view = "cmdline",
 			},
 			messages = {
-				enabled = false, -- Disable all messages except for search_count
-				view_search = "virtualtext", -- Enable virtualtext for search count messages
+				enabled = true,
+				view = false, -- default view for messages
+				view_error = false, -- view for errors
+				view_warn = false, -- view for warnings
+				view_history = false, -- view for :messages
+				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+			},
+			popupmenu = {
+				enabled = false,
+			},
+			redirect = { view = false },
+			commands = {
+				errors = { view = false },
+				all = { view = false },
+			},
+			notify = { enabled = false },
+			lsp = {
+				progress = { enabled = false },
+				hover = { enabled = false },
+				signature = { enabled = false },
+				message = { enabled = false },
+				-- defaults for hover and signature help
+			},
+			markdown = {
+				hover = {
+					["|(%S-)|"] = vim.cmd.help, -- vim help links
+					["%[.-%]%((%S-)%)"] = require("noice.util").open, -- markdown links
+				},
+				highlights = {
+					["|%S-|"] = "@text.reference",
+					["@%S+"] = "@parameter",
+					["^%s*(Parameters:)"] = "@text.title",
+					["^%s*(Return:)"] = "@text.title",
+					["^%s*(See also:)"] = "@text.title",
+					["{%S-}"] = "@parameter",
+				},
+			},
+			health = {
+				checker = false, -- Disable if you don't want health checks to run
+			},
+			presets = {
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = false, -- position the cmdline and popupmenu together
+				long_message_to_split = false, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 			routes = {
-				{
+				any = {
 					filter = {
 						event = "msg_show",
 						kind = "",
@@ -19,11 +64,6 @@ return {
 					},
 					opts = { skip = true },
 				},
-			},
-			presets = {
-				long_message_to_split = false, -- Disable long message splitting
-				inc_rename = false, -- Disable incremental renaming UI
-				lsp_doc_border = false, -- Disable LSP documentation border
 			},
 		})
 	end,
