595eeb0d0560afc6c0130cc024202a6fc71dcaae
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-31 04:22:20 +0900

    modified noice.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/noice.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/noice.lua
@@ -1,6 +1,14 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
 	config = function()
 		require("noice").setup({
 			cmdline = {
@@ -9,62 +17,78 @@ return {
 			},
 			messages = {
 				enabled = true,
-				view = false, -- default view for messages
+				view = "notify", -- default view for messages
 				view_error = "messages", -- view for errors
 				view_warn = "notify", -- view for warnings
 				view_history = "messages", -- view for :messages
 				view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
 			},
-			popupmenu = {
-				enabled = false,
-			},
-			redirect = { view = false },
-			commands = {
-				errors = { view = false },
-				all = { view = false },
-			},
-			notify = { enabled = false },
-			lsp = {
-				progress = { enabled = false },
-				hover = { enabled = false },
-				signature = { enabled = false },
-				message = { enabled = false },
-				-- defaults for hover and signature help
-			},
-			markdown = {
-				hover = {
-					["|(%S-)|"] = vim.cmd.help, -- vim help links
-					["%[.-%]%((%S-)%)"] = require("noice.util").open, -- markdown links
-				},
-				highlights = {
-					["|%S-|"] = "@text.reference",
-					["@%S+"] = "@parameter",
-					["^%s*(Parameters:)"] = "@text.title",
-					["^%s*(Return:)"] = "@text.title",
-					["^%s*(See also:)"] = "@text.title",
-					["{%S-}"] = "@parameter",
-				},
-			},
-			health = {
-				checker = false, -- Disable if you don't want health checks to run
-			},
-			presets = {
-				bottom_search = false, -- use a classic bottom cmdline for search
-				command_palette = false, -- position the cmdline and popupmenu together
-				long_message_to_split = false, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
 			routes = {
-				any = {
+				{
 					filter = {
 						event = "msg_show",
-						kind = "",
-						["not"] = { kind = { "search_count" } },
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+							{ find = "%d fewer lines" },
+							{ find = "%d more lines" },
+						},
 					},
 					opts = { skip = true },
 				},
 			},
 		})
 	end,
+	init = function()
+		local wk = require("which-key")
+		wk.add({
+			mode = { "n", "v" },
+			{ "<leader>n", group = "Noice" },
+		})
+	end,
+	keys = {
+		{
+			"<leader>nd",
+			function()
+				require("noice").cmd("dismiss")
+			end,
+			desc = "Dismiss",
+		},
+		{
+			"<leader>ne",
+			function()
+				require("noice").cmd("errors")
+			end,
+			desc = "Errors",
+		},
+		{
+			"<leader>nh",
+			function()
+				require("noice").cmd("history")
+			end,
+			desc = "History",
+		},
+		{
+			"<leader>nl",
+			function()
+				require("noice").cmd("last")
+			end,
+			desc = "Last",
+		},
+		{
+			"<leader>nm",
+			function()
+				require("noice").cmd("telescope")
+			end,
+			desc = "Messages",
+		},
+		{
+			"<leader>ns",
+			function()
+				require("noice").cmd("stats")
+			end,
+			desc = "Stats",
+		},
+	},
 }
