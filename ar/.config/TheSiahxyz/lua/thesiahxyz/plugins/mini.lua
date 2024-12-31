3d4d86bf0a0eceae4bbf5d6439e256d8f9b0fc6f
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2024-12-31 21:10:06 +0900

    modified mini.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/mini.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/mini.lua
@@ -1033,48 +1033,6 @@ return {
 			})
 		end,
 	},
-	-- {
-	-- 	"echasnovski/mini.jump",
-	-- 	version = false,
-	-- 	config = function()
-	-- 		require("mini.jump").setup( -- No need to copy this inside `setup()`. Will be used automatically.
-	-- 			{
-	-- 				-- Module mappings. Use `''` (empty string) to disable one.
-	-- 				mappings = {
-	-- 					forward = "f",
-	-- 					backward = "F",
-	-- 					forward_till = "t",
-	-- 					backward_till = "T",
-	-- 					repeat_jump = ";",
-	-- 				},
-	--
-	-- 				-- Delay values (in ms) for different functionalities. Set any of them to
-	-- 				-- a very big number (like 10^7) to virtually disable.
-	-- 				delay = {
-	-- 					-- Delay between jump and highlighting all possible jumps
-	-- 					highlight = 250,
-	--
-	-- 					-- Delay between jump and automatic stop if idle (no jump is done)
-	-- 					idle_stop = 10000000,
-	-- 				},
-	--
-	-- 				-- Whether to disable showing non-error feedback
-	-- 				-- This also affects (purely informational) helper messages shown after
-	-- 				-- idle time if user input is required.
-	-- 				silent = false,
-	-- 			}
-	-- 		)
-	--
-	-- 		-- Set MiniJump highlight initially
-	-- 		update_mini_jump_highlight()
-	--
-	-- 		-- Update MiniJump highlight whenever the colorscheme changes
-	-- 		vim.api.nvim_create_autocmd("ColorScheme", {
-	-- 			group = vim.api.nvim_create_augroup("MiniJumpHighlight", {}),
-	-- 			callback = update_mini_jump_highlight,
-	-- 		})
-	-- 	end,
-	-- },
 	{
 		"echasnovski/mini.map",
 		version = false,
