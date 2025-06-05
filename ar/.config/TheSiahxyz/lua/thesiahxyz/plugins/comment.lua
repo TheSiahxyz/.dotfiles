4a6be24c3994a80bf5a355d2eee4ebfaa4707e09
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-06-05 13:48:45 +0900

    modified plugins/comment.lua, modified plugins/mini.lua, modified plugins/todo.lua, created plugins/colorizer.lua

--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/comment.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/comment.lua
@@ -1,50 +1,8 @@
 return {
-	{
-		"numToStr/Comment.nvim",
-		lazy = false,
-		opts = {},
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-		cmd = { "TodoTrouble", "TodoTelescope" },
-		config = function()
-			require("todo-comments").setup()
-		end,
-		init = function()
-			local wk = require("which-key")
-			wk.add({
-				mode = { "n", "v" },
-				{ "<leader>t", group = "TODO" },
-			})
-		end,
-		keys = {
-			{
-				"]t",
-				function()
-					require("todo-comments").jump_next()
-				end,
-				desc = "Next Todo Comment",
-			},
-			{
-				"[t",
-				function()
-					require("todo-comments").jump_prev()
-				end,
-				desc = "Previous Todo Comment",
-			},
-			{ "<leader>tt", "<cmd>Trouble todo toggle<cr>", desc = "Toggle TODO (Trouble)" },
-			{
-				"<leader>tT",
-				"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
-				desc = "Toggle Todo/Fix/Fixme (Trouble)",
-			},
-			{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find Todo" },
-			{ "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find Todo/Fix/Fixme" },
-		},
-	},
+	"numToStr/Comment.nvim",
+	lazy = false,
+	opts = {},
+	config = function()
+		require("Comment").setup()
+	end,
 }
--- ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/todo.lua
+++ ar/.config/TheSiahxyz/lua/thesiahxyz/plugins/todo.lua
@@ -1,14 +1,56 @@
 return {
-	"vimichael/floatingtodo.nvim",
-	config = function()
-		require("floatingtodo").setup({
-			target_file = "~/.local/share/vimwiki/todo.md",
-			border = "single", -- single, rounded, etc.
-			width = 0.8, -- width of window in % of screen size
-			height = 0.8, -- height of window in % of screen size
-			position = "center", -- topleft, topright, bottomleft, bottomright
-		})
+	{
+		"vimichael/floatingtodo.nvim",
+		config = function()
+			require("floatingtodo").setup({
+				target_file = "~/.local/share/vimwiki/todo.md",
+				border = "single", -- single, rounded, etc.
+				width = 0.8, -- width of window in % of screen size
+				height = 0.8, -- height of window in % of screen size
+				position = "center", -- topleft, topright, bottomleft, bottomright
+			})
 
-		vim.keymap.set("n", "<leader>tf", ":Td<CR>", { silent = true, desc = "Todo floating" })
-	end,
+			vim.keymap.set("n", "<leader>tf", ":Td<CR>", { silent = true, desc = "TODO floating" })
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		config = function()
+			require("todo-comments").setup()
+		end,
+		init = function()
+			local wk = require("which-key")
+			wk.add({
+				mode = { "n", "v" },
+				{ "<leader>t", group = "TODO" },
+			})
+		end,
+		keys = {
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Next Todo Comment",
+			},
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+				desc = "Previous Todo Comment",
+			},
+			{ "<leader>tt", "<cmd>Trouble todo toggle<cr>", desc = "Toggle TODO (Trouble)" },
+			{
+				"<leader>tT",
+				"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
+				desc = "Toggle Todo/Fix/Fixme (Trouble)",
+			},
+			{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find Todo" },
+			{ "<leader>fT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Find Todo/Fix/Fixme" },
+		},
+	},
 }
