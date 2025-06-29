6d4a3866ff2f5ecb0375d70a0a0d694b066a64df
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-06-29 15:52:38 +0900

    updates

--- ar/.config/TheSiahxyz/lua/TheSiahxyz/plugins/colorizer.lua
+++ ar/.config/TheSiahxyz/lua/TheSiahxyz/plugins/colorizer.lua
@@ -34,7 +34,7 @@ return {
 				RGBA = true, -- #RGBA hex codes
 				RRGGBB = true, -- #RRGGBB hex codes
 				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				AARRGGBB = true, -- 0xAARRGGBB hex codes
+				AARRGGBB = false, -- 0xAARRGGBB hex codes
 				rgb_fn = true, -- CSS rgb() and rgba() functions
 				hsl_fn = true, -- CSS hsl() and hsla() functions
 				css = true, -- Enable all CSS *features*:
