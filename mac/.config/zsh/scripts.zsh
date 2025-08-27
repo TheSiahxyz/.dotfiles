c8892cc179733a0d44711cced9b51ce806bae086
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-08-28 08:51:20 +0900

    changes

--- mac/.config/zsh/scripts.zsh
+++ mac/.config/zsh/scripts.zsh
@@ -466,7 +466,7 @@ function fzf_kill_process() {
 # open lf and cd to the file path
 function lfcd () {
     tmp="$(mktemp -uq)"
-    trap 'rm -f $tmp >/dev/null 2>&1 && trap - HUP INT QUIT TERM PWR EXIT' HUP INT QUIT TERM PWR EXIT
+    trap 'rm -f $tmp >/dev/null 2>&1 && trap - HUP INT QUIT TERM EXIT' HUP INT QUIT TERM EXIT
     lf -last-dir-path="$tmp" "$@"
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
