e129395dada177f359d41cf80967f59a1b1f0f3c
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-08-30 06:54:29 +0900

    modified items/battery.sh, modified items/calendar.sh, modified plugins/calendar.sh

--- mac/.config/sketchybar/items/battery.sh
+++ mac/.config/sketchybar/items/battery.sh
@@ -9,8 +9,8 @@ battery=(
   update_freq=120
   updates=on
 )
+
 sketchybar --add item battery right \
            --set battery "${battery[@]}"\
               icon.font.size=15 update_freq=120 script="$PLUGIN_DIR/battery.sh" \
            --subscribe battery power_source_change system_woke
-
