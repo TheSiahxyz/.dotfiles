fa93da5e5a6b2a9c4f9a4b0b0070465e292c2124
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-08-29 22:00:27 +0900

    modified plugins/youtube-music.sh, modified sketchybar/sketchybarrc

--- mac/.config/sketchybar/plugins/youtube-music.sh
+++ mac/.config/sketchybar/plugins/youtube-music.sh
@@ -5,12 +5,13 @@ SONG_INFO=$(curl -s 0.0.0.0:26538/api/v1/song-info)
 PAUSED="$(echo "$SONG_INFO" | jq -r '.isPaused')"
 CURRENT_SONG="$(echo "$SONG_INFO" | jq -r '.title + " - " + .artist')"
 ARTWORK="$(echo "$SONG_INFO" | jq -r '.imageSrc')"
-ARTWORK_LOCATION="$(curl -O --output-dir "$TMPDIR" -s --remote-name -w "%{filename_effective}" "$ARTWORK")"
+ARTWORK_LOCATION="$(curl -O --output-dir "${TMPDIR%/}" -s --remote-name -w "%{filename_effective}" "$ARTWORK")"
 
 if [ "$PAUSED" = true ]; then
   ICON=􀊄
 else
   ICON=􁁒
 fi
-sketchybar --set "$NAME" label="$CURRENT_SONG" icon="$ICON" drawing=on
-sketchybar --set "$NAME"-artwork background.image="$ARTWORK_LOCATION"
+
+sketchybar --set music-artwork background.image="$ARTWORK_LOCATION"
+sketchybar --set music label="$CURRENT_SONG" icon="$ICON" drawing=on
