68ed1a68693ee7eb1cf76bf93028ef7dd43939ee
TheSiahxyz <164138827+TheSiahxyz@users.noreply.github.com>
2025-09-13 23:04:01 +0900

    deleted plugins/lf.lua, created plugins/yazi.lua, created plugins/yazi.lua

---
+++ ar/.config/TheSiahxyz/lua/TheSiahxyz/plugins/yazi.lua
@@ -0,0 +1,47 @@
+return {
+	"mikavilpas/yazi.nvim",
+	version = "*", -- use the latest stable version
+	event = "VeryLazy",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", lazy = true },
+	},
+	keys = {
+		{
+			"<leader>ey",
+			mode = { "n", "v" },
+			"<cmd>Yazi<cr>",
+			desc = "Open yazi at the current file",
+		},
+		{
+			-- Open in the current working directory
+			"<leader>eY",
+			"<cmd>Yazi cwd<cr>",
+			desc = "Open the file manager in nvim's working directory",
+		},
+		{
+			"zy",
+			"<cmd>Yazi toggle<cr>",
+			desc = "Resume the last yazi session",
+		},
+	},
+	---@type YaziConfig | {}
+	opts = {
+		-- if you want to open yazi instead of netrw, see below for more info
+		open_for_directories = false,
+		keymaps = {
+			show_help = "<f1>",
+		},
+	},
+	-- 👇 if you use `open_for_directories=true`, this is recommended
+	init = function()
+		-- mark netrw as loaded so it's not loaded at all.
+		--
+		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+		vim.g.loaded_netrwPlugin = 1
+		local wk = require("which-key")
+		wk.add({
+			mode = { "n" },
+			{ "<leader>ey", group = "Yazi" },
+		})
+	end,
+}
